feaba5b6e3119b9e3b5a5f2f368132bd8cbf2068
Alex Moreira <alex.moreira@server.signainfo.com.br>
2021-01-01 03:37:31 -0300

    feat: card, handleClick function

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,65 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <v-container>
+      <v-row>
+        <v-col cols="12" lg="3" v-for="(x, index) in 8" :key="index">
+          <Card
+            @selected="handleClick($event)"
+            :index="index"
+            :disabled="disabled"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import Card from "@/components/Card";
+
 export default {
+  data: () => ({
+    cardsChosen: [],
+    disabled: false,
+  }),
+
   name: "App",
+  components: { Card },
+
+  methods: {
+    handleClick(e) {
+      let { cardsChosen } = this;
+      if (cardsChosen.length < 2) {
+        this.disabled = false;
+        cardsChosen.push(e);
+      }
+
+      if (cardsChosen.length === 2) {
+        this.disabled = true;
+        if (cardsChosen[0].value === cardsChosen[1].value) {
+          setTimeout(() => {
+            this.correct();
+          }, 2000);
+        } else {
+          setTimeout(() => {
+            this.reset();
+          }, 2000);
+        }
+      }
+    },
+
+    correct() {
+      this.cardsChosen = [];
+      this.disabled = false;
+    },
+
+    reset() {
+      this.cardsChosen[0].chosen = false;
+      this.cardsChosen[1].chosen = false;
+      this.cardsChosen = [];
+      this.disabled = false;
+    },
+  },
 };
 </script>
 
@@ -16,5 +71,11 @@ export default {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+}
+
+#app {
+  height: 100vh;
+  // background: #0a0e42;
+  background: rgb(148, 131, 131);
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-card
+    flat
+    outlined
+    class="card"
+    :class="cards[index].chosen ? 'card card--selected' : 'card'"
+    :disabled="disabled"
+    height="300px"
+    @click="handleClick"
+  >
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    cards: [
+      {
+        id: 1,
+        value: "1",
+        chosen: false,
+      },
+      {
+        id: 2,
+        value: "1",
+        chosen: false,
+      },
+      {
+        id: 3,
+        value: "2",
+        chosen: false,
+      },
+      {
+        id: 4,
+        value: "2",
+        chosen: false,
+      },
+      {
+        id: 5,
+        value: "3",
+        chosen: false,
+      },
+      {
+        id: 6,
+        value: "3",
+        chosen: false,
+      },
+      {
+        id: 7,
+        value: "4",
+        chosen: false,
+      },
+      {
+        id: 8,
+        value: "4",
+        chosen: false,
+      },
+    ],
+  }),
+
+  props: {
+    index: Number,
+    disabled: Boolean,
+  },
+
+  methods: {
+    handleClick() {
+      this.selectCards();
+    },
+
+    selectCards() {
+      if (!this.cards[this.index].chosen) {
+        this.cards[this.index].chosen = true;
+        this.$emit("selected", this.cards[this.index]);
+      } else {
+        this.cards[this.index].chosen = false;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  background: yellow !important;
+  border: 2px solid #fff !important;
+  width: 250px;
+
+  &--selected {
+    background: blue !important;
+  }
+}
+</style>
